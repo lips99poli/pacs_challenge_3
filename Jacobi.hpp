669e51e916e32580804d7a0659d4f482e2a4203c
lips99poli <filippo.lipari@mail.polimi.it>
2024-06-02 16:26:36 +0000

    working tests, still have to check correctness

--- Jacobi.hpp
+++ Jacobi.hpp
@@ -1,6 +1,7 @@
 #ifndef JACOBI_HPP
 #define JACOBI_HPP
 
+#include <iostream>
 #include <vector>
 #include <Eigen/Dense>
 #include <functional>
@@ -18,13 +19,16 @@
 
 template <typename T>
 struct BoundaryCondition{
-    unsigned int grid_row;
-    unsigned int grid_col;
+    unsigned int grid_row=100;
+    unsigned int grid_col=100;
 
-    std::function<T(unsigned int i, unsigned int j)> nord;
-    std::function<T(unsigned int i, unsigned int j)> est;
-    std::function<T(unsigned int i, unsigned int j)> sud;
-    std::function<T(unsigned int i, unsigned int j)> ovest;
+    std::function<T(unsigned int i, unsigned int j)> nord=nullptr;
+    std::function<T(unsigned int i, unsigned int j)> est=nullptr;
+    std::function<T(unsigned int i, unsigned int j)> sud=nullptr;
+    std::function<T(unsigned int i, unsigned int j)> ovest=nullptr;
+
+    BoundaryCondition(unsigned int grid_row, unsigned int grid_col, std::function<T(unsigned int i, unsigned int j)> nord, std::function<T(unsigned int i, unsigned int j)> est, std::function<T(unsigned int i, unsigned int j)> sud, std::function<T(unsigned int i, unsigned int j)> ovest) : grid_row(grid_row), grid_col(grid_col), nord(nord), est(est), sud(sud), ovest(ovest){};
+    BoundaryCondition() = default;
 };
 
 template <typename T>
@@ -45,14 +49,17 @@ class Jacobi {
     T h;
     T h_2;
 
+    // Common members
     Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> grid; // U
-    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> function_grid; //f(U)
+    // Local members
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> local_function_grid; //f(U)
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> local_grid;
+    Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> new_local_grid;
+    unsigned int first_row;
 
     // MPI Variables are members since it is a parallel solver
     int rank;
     int size;
-
-
 
     // Boundary Condition
     BoundaryCondition<T> bc;
@@ -61,30 +68,44 @@ class Jacobi {
     std::function<T(T,T)> f;
 
     // Save the error
-    T error;
+    T error = 0.;
 
     public:
-    Jacobi(unsigned int maxIt=1e3, T tol=1e-6, unsigned int N=1e3) : maxIt(maxIt), tol(tol), N(N), grid(N,N), function_grid(N-1,N-1){
+    Jacobi(unsigned int maxIt=1e3, T tol=1e-6, unsigned int N=11) : maxIt(maxIt), tol(tol), N(N), grid(N,N){
+        // Initialize MPI variables
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
+
         grid.setZero();
         h = (xN-x0)/N; //se lo lasci così lo spacing è omogeneo lungo x e y
         h_2 = h*h;
 
-        // Initialize MPI variables
-        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-        MPI_Comm_size(MPI_COMM_WORLD, &size);
-        };
+        // Create local grid: rows block partitioned
+        // rank 0 and rank size-1 have 1 more row to contain respectively row under and row above,
+        // All other ranks have 2 more rows to contain both row_under and row_above
+        // These rows will be updated every time before the iteration start, with the infromation coming from adjacent ranks
+        unsigned int local_rows = (N%size > rank) ? N/size + 1 + 2 : N/size + 2;
+        if (rank==0 || rank==size-1) --local_rows;
 
-    void setFunction(std::function<T(T,T)>& f){
+        first_row = N/size * rank + std::min(rank, static_cast<int> (N%size));
+        
+        local_grid.resize(local_rows, N);
+        local_grid.setZero();
+
+        // Create local function grid: for each rank i need the same rows that I have in the local grid, excluding the boundaries and row_above/row_under
+        unsigned int function_local_rows = local_rows - 2;
+        local_function_grid.resize(function_local_rows, N-2);
+        }
+
+
+    void setFunction(std::function<T(T,T)> f){
         this->f = f;
 
-        // Parallel Initialization of the function grid
-        unsigned int local_rows = (N-2)%size > rank ? (N-2)/size + 1 : (N-2)/size;
-        unsigned int cols = N-2;
-        Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> local_function_grid(local_rows, cols);
-        local_function_grid.setZero();
-
+        // Initialization of the local function grid
         // Each rank has to know what is its first row w.r.t. original grid
-        unsigned int first_row = (N-2)/size * rank + std::min(rank, (N-2)%size); // or unsigned int first_row = ((N-2)%size > rank) ? (N-2)/size * rank + rank  : (N-2)/size * rank + (N-2)%size;        
+        unsigned int cols = N-2;
+        unsigned int local_rows = local_function_grid.rows();
+        unsigned int first_row = (N-2)/size * rank + std::min(rank, static_cast<int> (N-2)%size); // or unsigned int first_row = ((N-2)%size > rank) ? (N-2)/size * rank + rank  : (N-2)/size * rank + (N-2)%size;        
 
         // Fill local matrix, recall that w.r.t. the full grid we avoid the boundaries so sum 1 in the indexes
         for(Eigen::Index i=0; i<local_rows; ++i){
@@ -93,46 +114,40 @@ class Jacobi {
             }
         }
 
-        // Define the things i need to Allgather the results
+        /* Check of the comunication
+        Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> function_grid(N-2, N-2);
         std::vector<int> displs(size);
         std::vector<int> sendcounts(size);
         int sum = 0;
         for (int i = 0; i < size; ++i) {
             sendcounts[i] = (N-2)%size > i ? (N-2)/size + 1 : (N-2)/size;
+            sendcounts[i] *= N-2;
             displs[i] = sum;
             sum += sendcounts[i];
         }
-
         // Copy the local grid to the global grid
         MPI_Allgatherv(local_function_grid.data(), local_rows*(N-2), MPI_DOUBLE, function_grid.data(), sendcounts.data(), displs.data(), MPI_DOUBLE, MPI_COMM_WORLD);
-
+        if (rank==0){
+            std::cout << function_grid << std::endl;
+        }
+        */
     }
 
     void setBoundaryCondition(BoundaryCondition<T>& bc){
         this->bc = bc;
-    };
-
-    bool solve(){
-
-        // Create local grid: rows block partitioned
-        // rank 0 and rank size-1 have 1 more row to contain respectively row under and row above,
-        // All other ranks have 2 more rows to contain both row_under and row_above
-        // These rows will be updated every time before the iteration start, with the infromation coming from adjacent ranks
-        unsigned int cols = grid.cols();
-        unsigned int local_rows = (grid.rows()%size > rank) ? grid.rows()/size + 1 + 2 : grid.rows()/size + 2;
-        if (rank==0 || rank==size-1) --local_rows;
-
-        unsigned int first_row = N/size * rank + std::min(rank, N%size);
-
-        Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> local_grid(local_rows, cols);
-        local_grid.setZero();
 
         // Each rank has to apply boundary condition on its local grid
-        apply_boundary_condtion(local_grid, rank, size);
+        apply_boundary_condtion();
 
         // We need a container with the updates
-        Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> new_local_grid = local_grid;
+        new_local_grid = local_grid;
+    };
 
+
+    void solve(){
+        unsigned int cols = local_grid.cols();
+        unsigned int local_rows = local_grid.rows();
+    
         // The goal is performance: in the first iteration i don't need any comuncation, which is needed at the beginning of every iteration, AFTER THE CONVERGENCE CHECK!
         // In order to avoid useless communication i want to do the for loop starting with the communication and then perform updates and finally check convergence. But since the 
         // the first communication is useless I perform the first iteration outside and then enter the loop
@@ -143,7 +158,7 @@ class Jacobi {
         // Jacobi iteration
         bool go_on = true;
         for (unsigned int it = 0; go_on; ++it) {
-            communicate_rows(local_grid, rank, size);
+            communicate_rows(local_grid);
             
             // if (rank==0) I shouldn't touch the first row and the first and last columns
             // if(rank==size-1) I shouldn't touch the last row and the first and last columns
@@ -160,7 +175,7 @@ class Jacobi {
                     // Update
                     new_local_grid(i, j) = 0.25 * (
                         local_grid(i-1, j) + local_grid(i+1, j) + local_grid(i, j-1) + local_grid(i, j+1) + 
-                        h_2 * function_grid(first_row + i -1, j-1)
+                        h_2 * local_function_grid(i-1, j-1)
                     );
                 }
             }
@@ -173,39 +188,57 @@ class Jacobi {
         std::vector<int> sendcounts(size);
         int sum = 0;
         for (int i = 0; i < size; ++i) {
-            sendcounts[i] = (grid.rows()%size > i) ? grid.rows()/size + 1 : grid.rows()/size;
+            sendcounts[i] = (N%size > i) ? (N/size + 1)*cols : (N/size)*cols;
             displs[i] = sum;
             sum += sendcounts[i];
         }
 
-        // Copy the local grid to the global grid
+        // The first line shouldn't be sent, a part from rank 0
         unsigned offset = (rank==0)? 0 : cols;
-        unsigned count = (rank==0 || rank==size-1)? local_rows-1 : local_rows-2;
-        MPI_Allgatherv(local_grid.data() + offset, count, MPI_DOUBLE, grid.data(), sendcounts.data(), displs.data(), MPI_DOUBLE, MPI_COMM_WORLD);
+
+        // Copy the local grid to the global grid
+        MPI_Allgatherv(local_grid.data() + offset, sendcounts[rank], MPI_DOUBLE, grid.data(), sendcounts.data(), displs.data(), MPI_DOUBLE, MPI_COMM_WORLD);
+    }
+
+    void print() const {
+        //std::cout << grid << std::endl;
+
+        // Print the local matrix in order w.r.t. rank number
+        for (int i = 0; i < size; ++i) {
+            if (rank == i) {
+                std::cout << "Rank " << rank << std::endl;
+                std::cout << new_local_grid << std::endl;
+            }
+            MPI_Barrier(MPI_COMM_WORLD);
+        }
+        std::cout << std::endl;
+        // Print the global matrix only from rank 0
+        if (rank == 0) {
+            std::cout << "Entire grid" << grid << std::endl;
+        }
     }
 
     private:
 
-    void apply_boundary_condtion(Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic, Eigen::RowMajor>& local_grid, int r, int s) const {
-        unsigned int first_row = N/size * rank + std::min(rank, N%size);
-        if(r==0){// Nord
-            for(unsigned int j=0; j<grid.cols(); j++){
-                local_grid(0,j) = bc.nord(0,j);
+    void apply_boundary_condtion() {
+        // Recall that in local_grid there are also row_above and row_under, so ignore them with proper indexing
+        for(unsigned int i=1; i<local_grid.rows()-1; ++i){ // Est Ovest
+            local_grid(i,0) = 0.;
+            local_grid(i,local_grid.cols()-1) = 0.;
+        }
+        // Moreover we have upper and lower boundaries
+        if(rank==0){// Nord
+            for(unsigned int j=0; j<local_grid.cols(); ++j){
+                local_grid(0,j) = 0.;
             }
-        }else if(r==s-1){// Sud
-            for(unsigned int j=0; j<j<grid.cols(); j++){
-                local_grid(local_grid.rows()-1,j) = bc.sud(grid.rows(),j);
-            }
-        }else{ // Est Ovest
-            // Recall that in local_grid there are also row_above and row_under, so ignore them with proper indexing
-            for(unsigned int i=1; i<local_grid.rows()-1; i++){
-                local_grid(i,0) = bc.ovest(i,0);
-                local_grid(i,grid.cols()-1) = bc.est(first_row + i,grid.cols()-1);
+        }else if(rank==size-1){// Sud
+            for(unsigned int j=0; j<local_grid.cols(); ++j){
+                local_grid(local_grid.rows()-1,j) = 0.;
             }
         }
     };
 
-    void comunicate_rows(Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic, Eigen::RowMajor>& local_grid){
+    void communicate_rows(Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic, Eigen::RowMajor>& local_grid){
         unsigned int local_rows = local_grid.rows();
         unsigned int cols = local_grid.cols();
 
@@ -239,14 +272,14 @@ class Jacobi {
                         MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
         */
-
+        
         // Synchronize all ranks before proceeding to the next step
         MPI_Barrier(MPI_COMM_WORLD);
     };
 
     // Alternative with SendRecv 
 
-    bool check_convergence(const Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic, Eigen::RowMajor>& local_grid, const Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic, Eigen::RowMajor>& new_local_grid)const {
+    bool check_convergence(const Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic, Eigen::RowMajor>& local_grid, const Eigen::Matrix<T,Eigen::Dynamic,Eigen::Dynamic, Eigen::RowMajor>& new_local_grid) {
         error = 0.;
         
         // Compute the error, i don't need to check the first and last row because they are either boundary conditions or they are updated by other ranks
@@ -260,6 +293,7 @@ class Jacobi {
         error = std::sqrt(h*error);
         return error < tol;
     }
+
 };
 
 #endif
